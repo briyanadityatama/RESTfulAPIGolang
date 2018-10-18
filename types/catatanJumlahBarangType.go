82c5be6f97e2132ffc0aede0105d45a267416ab5
Briyan Adityatama <briyansykes@yahoo.com>
2018-10-18 15:23:25 +0700

    refactor code catatanJumlahBarangType

--- types/catatanJumlahBarangType.go
+++ types/catatanJumlahBarangType.go
@@ -7,29 +7,6 @@ import (
 	"strconv"
 )
 
-// const dateFormat = "2006-01-02 15:04:05"
-
-// type MyTime struct {
-// 	time.Time
-// }
-
-// func (m *MyTime) UnmarshalJSON(p []byte) error {
-// 	t, err := time.Parse(dateFormat, strings.Replace(
-// 		string(p),
-// 		"\"",
-// 		"",
-// 		-1,
-// 	))
-
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	m.Time = t
-
-// 	return nil
-// }
-
 var headerJumlahBarang = []string{`sku`, `nama_item`, `jumlah_sekarang`}
 
 type (
